f563577829bf9437871793c6166516c5b0d5cfd4
asd0905 <dydcodydco@daum.net>
2024-09-29 16:51:14 +0900

    page에서 params, searchParams 확인

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -1,4 +1,11 @@
-export default function PhotoModal({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function PhotoModal({ params, searchParams }: Props) {
+  console.log('searchParams : ', searchParams);
+  console.log('params : ', params);
   return (
     <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', border: '1px solid black' }}>
       <h2>사진 {params.id} 모달</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,12 @@ export default function Home() {
       <h1>홈페이지</h1>
       <nav>
         <ul>
-          <li><Link href="/dashboard">대시보드</Link></li>
+          <li><Link href="/dashboard?search=1">대시보드</Link></li>
           <li><Link href="/photos/1">사진 1</Link></li>
+          <li><Link href={{
+            pathname: '/photos/1',
+            query: { search: '1' }
+          }}>사진 1 + query string</Link></li>
           <li><Link href="/photos/2">사진 2</Link></li>
           <li><Link href="/photos/2/3">사진 2,3</Link></li>
         </ul>
