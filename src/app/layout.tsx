e85580ef6205b4a7823b007163d0cdabdd8964a8
asd0905 <dydcodydco@daum.net>
2024-09-29 16:50:51 +0900

    layout 공통 해드 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>공통 해드</h1>
         {children}
         {modal}
       </body>
